5e66e43df05fafd92e123cccfe422c549b9cc7aa
mudaseer20 <mudaseer20@gmail.com>
2026-09-22 14:12:55 +0530

    updated code

--- src/test/java/runner/RunCuke.java
+++ src/test/java/runner/RunCuke.java
@@ -3,7 +3,7 @@ package runner;
 
 import io.cucumber.testng.*;
 
-@CucumberOptions(features="src/test/resources/features/kapoorclearance.feature", monochrome = true,glue= {"steps"},
+@CucumberOptions(features="src/test/resources/features/vases.feature", monochrome = true,glue= {"steps"},
 
 plugin= {"html:target/cucumber-reports/cucumber-html-reports.html","pretty"})
 public class RunCuke extends AbstractTestNGCucumberTests{
