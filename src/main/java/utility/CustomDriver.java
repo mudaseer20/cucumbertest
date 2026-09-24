50b50bfe238161df1a21135a0c3da29e625ec7e8
mudaseer20 <43870075+mudaseer20@users.noreply.github.com>
2026-09-24 17:44:45 +0530

    Enhance elementClick method with retry logic
    Refactor elementClick method to handle StaleElementReferenceException and retry clicking the element up to 3 times.

--- src/main/java/utility/CustomDriver.java
+++ src/main/java/utility/CustomDriver.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -189,6 +190,8 @@ public List<String> getelement(List<WebElement> el){
 }
 
 public void elementClick(List<WebElement> el,String text) {
+	 int attempts = 0;
+	    while (attempts < 3) {
 	try {
 	       WebElement elementToClick = el.stream()
 	               .filter(product -> product.getText().trim().toLowerCase().contains(text.toLowerCase()))
@@ -197,20 +200,26 @@ public void elementClick(List<WebElement> el,String text) {
 	       WebDriverWait wait = new WebDriverWait(SeleniumDriver.getDriver(), Duration.ofSeconds(10));
 	     wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(elementToClick)));
 	           elementToClick.click();
+	           break;
 	}
-	catch(Exception e) {
-		  WebElement elementToClick = el.stream()
-	               .filter(product -> product.getText().trim().toLowerCase().contains(text.toLowerCase()))
-	               .findFirst()
-	               .orElseThrow(() -> new NoSuchElementException("Element with text '" + text + "' not found in the list."));
-	       WebDriverWait wait = new WebDriverWait(SeleniumDriver.getDriver(), Duration.ofSeconds(10));
-	     wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(elementToClick)));
-	           elementToClick.click();
+	
+	catch(StaleElementReferenceException e) {
+		
+		 
+	
 	
 	}
 	
-	
+
+	   catch (Exception e) {
+  
+ 
+}
+
+
+	 attempts++;
 	
 	
 }
-}
+	   
+}}
